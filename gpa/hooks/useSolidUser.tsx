3bd90d1803c5dc2331b7ed88c27cd4b8cf86df9f
Gergely Tóth <gergelytothdev@gmail.com>
2021-01-23 13:06:13 +0100

    Working SWR with user Solid Pod

--- gpa/hooks/useSolidUser.tsx
+++ gpa/hooks/useSolidUser.tsx
@@ -7,13 +7,19 @@ export function useUserName(): {
   isLoading: boolean;
   isError: boolean;
 } {
-  const fetcher = (field: string): Promise<string> => {
+  const fetcher = (_: string, field: string): Promise<string> => {
     return GetField(field).then((res) => res);
   };
 
   // here we have to care that this key in useSWR has to be unique across the whole application (to handle caching)
   // so perhaps use more descriptive name, such as "solid-firstName" or whatever
-  const { data, error } = useSWR(fieldToRdfMap.firstName, fetcher);
+
+  // if we specify key (first argument of useSWR) as an array, the ID for caching will be calculated for the combination of the elements
+  // https://swr.vercel.app/docs/arguments
+  const { data, error } = useSWR(
+    ["user_solid", fieldToRdfMap.firstName],
+    fetcher
+  );
 
   return {
     userName: data,
--- gpa/pages/api/name.tsx
+++ gpa/pages/api/name.tsx
@@ -1,3 +1,5 @@
+//OBSOLETE - kept only as API reference
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { GetField } from "../../util/solid";
