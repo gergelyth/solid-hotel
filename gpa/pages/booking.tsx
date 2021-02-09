7f40ecd36ec63ee4507b9cc4de1d4b5f37a88a72
Gergely Tóth <gergelytothdev@gmail.com>
2021-02-09 23:51:07 +0100

    [GPA][Booking] Page refactor

--- gpa/pages/booking.tsx
+++ gpa/pages/booking.tsx
@@ -1,6 +1,7 @@
 import dynamic from "next/dynamic";
 import Head from "next/head";
-import React from "react";
+import { useRouter } from "next/router";
+import { Dispatch, SetStateAction, useState } from "react";
 import styles from "../styles/Home.module.css";
 import { GetCurrentDatePushedBy } from "../test/setup/populateHotelPod/withReservations";
 import { ReservationState } from "../types/ReservationState";
@@ -16,75 +17,84 @@ const DynamicRoomSelector = dynamic(
   () => import("../components/booking/room-selector")
 );
 
-class Booking extends React.Component<unknown, { currentPage: BookingPage }> {
-  constructor(props: unknown) {
-    super(props);
-    this.state = { currentPage: BookingPage.ReservationProperties };
-  }
+function BookRoom(roomId: string, checkinDate: Date, checkoutDate: Date): void {
+  const reservation = {
+    id: roomId,
+    ownerId: 10,
+    roomId: 0,
+    state: ReservationState.CONFIRMED,
+    dateFrom: checkinDate,
+    dateTo: checkoutDate,
+  };
 
-  BookRoom(roomId: string, checkinDate: Date, checkoutDate: Date): void {
-    const reservation = {
-      id: roomId,
-      ownerId: 10,
-      roomId: 0,
-      state: ReservationState.CONFIRMED,
-      dateFrom: checkinDate,
-      dateTo: checkoutDate,
-    };
+  AddReservation(reservation);
+}
 
-    AddReservation(reservation);
-    this.setState({ currentPage: BookingPage.Success });
-  }
+function ReservationPropertiesPage(
+  currentPage: BookingPage,
+  setCurrentPage: Dispatch<SetStateAction<BookingPage>>
+): JSX.Element {
+  return (
+    <div>
+      <DynamicRoomSelector />
+      <p>==============================================</p>
+      <i>Add date picker and CSS for actual room select here.</i>
+      <i>
+        For now it will select the first room and pick dates a few dates in the
+        future.
+      </i>
+      <button
+        onClick={() => {
+          BookRoom(
+            "reservation100",
+            GetCurrentDatePushedBy(0, 0, 5),
+            GetCurrentDatePushedBy(0, 0, 10)
+          );
+          setCurrentPage(currentPage + 1);
+        }}
+      >
+        Book room
+      </button>
+    </div>
+  );
+}
 
-  ReservationPropertiesPage(): JSX.Element {
-    return (
-      <div>
-        <DynamicRoomSelector />
-        <p>==============================================</p>
-        <i>Add date picker and CSS for actual room select here.</i>
-        <i>
-          For now it will select the first room and pick dates a few dates in
-          the future.
-        </i>
-        <button
-          onClick={() => {
-            this.BookRoom(
-              "reservation100",
-              GetCurrentDatePushedBy(0, 0, 5),
-              GetCurrentDatePushedBy(0, 0, 10)
-            );
-          }}
-        >
-          Book room
-        </button>
-      </div>
-    );
-  }
+function SuccessPage(): JSX.Element {
+  const router = useRouter();
 
-  SuccessPage(): JSX.Element {
-    return <h2>Reservation successful!</h2>;
-  }
+  return (
+    <div>
+      <h2>Reservation successful!</h2>
+      <button onClick={() => router.push("/")}>Return to index page</button>
+    </div>
+  );
+}
 
-  DisplayPage(): JSX.Element {
-    switch (this.state.currentPage) {
-      case BookingPage.ReservationProperties:
-        return this.ReservationPropertiesPage();
-      case BookingPage.Success:
-        return this.SuccessPage();
-    }
+function DisplayPage(
+  currentPage: BookingPage,
+  setCurrentPage: Dispatch<SetStateAction<BookingPage>>
+): JSX.Element {
+  switch (currentPage) {
+    case BookingPage.ReservationProperties:
+      return ReservationPropertiesPage(currentPage, setCurrentPage);
+    case BookingPage.Success:
+      return SuccessPage();
   }
+}
 
-  render(): JSX.Element {
-    return (
-      <div className={styles.container}>
-        <Head>
-          <title>Book a room</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        {this.DisplayPage()}
-      </div>
-    );
-  }
+function Booking(): JSX.Element {
+  const [currentPage, setCurrentPage] = useState(
+    BookingPage.ReservationProperties
+  );
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Book a room</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {DisplayPage(currentPage, setCurrentPage)}
+    </div>
+  );
 }
 
 export default Booking;
