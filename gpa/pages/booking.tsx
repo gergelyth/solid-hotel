c60507c12d23fe1e738381f3789d62483af7570b
Gergely Tóth <gergelytothdev@gmail.com>
2021-02-17 22:54:22 +0100

    [GPA][Booking] Move success page

--- gpa/pages/booking.tsx
+++ gpa/pages/booking.tsx
@@ -57,7 +57,7 @@ function ReservationPropertiesPage(
             GetCurrentDatePushedBy(0, 0, 10)
           );
           // setCurrentPage(currentPage + 1);
-          router.push("/success");
+          router.push("/booking/success");
         }}
       >
         Book room
