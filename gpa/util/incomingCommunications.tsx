9d7a800b273a0d995aaebba21d45637f5a54b3a0
Gergely Tóth <gergelytothdev@gmail.com>
2021-06-09 01:29:36 +0200

    [GPA][Notifications] Added failure report parser

--- gpa/util/incomingCommunications.tsx
+++ gpa/util/incomingCommunications.tsx
@@ -2,6 +2,7 @@ import { SolidDataset } from "@inrupt/solid-client";
 import { SetReservationStateAndInbox } from "../../common/util/solid";
 import { NextRouter } from "next/router";
 import { DeserializeReservationStateChange } from "../../common/notifications/ReservationStateChange";
+import { DeserializeFailureReport } from "../../common/notifications/FailureReport";
 
 export function ReceiveReservationStateChange(
   router: NextRouter,
@@ -25,6 +26,33 @@ export function ReceiveReservationStateChange(
   const onReceive = (): void => {
     //TODO we should set the page in VerifyingComponent in different workflows so they don't wait - but how
     SetReservationStateAndInbox(reservationId, newState, replyInbox);
+  };
+
+  return { text, onClick, onReceive };
+}
+
+export function ReceiveFailureReport(
+  router: NextRouter,
+  url: string,
+  dataset: SolidDataset
+): {
+  text: string;
+  onClick: (event: React.MouseEvent<EventTarget>) => void;
+  onReceive: () => void;
+} {
+  const { reservationId, errorMessage, resultState } = DeserializeFailureReport(
+    url,
+    dataset
+  );
+  const text = `There was an error processing the reservation state change request: ${errorMessage}.
+        The state is left at ${resultState.toString()}.
+        Click to view reservation.`;
+  const onClick = (): void => {
+    router.push(`/reservations/${encodeURIComponent(reservationId)}`);
+  };
+  const onReceive = (): void => {
+    //if we don't set the ReservationState of the reservation to something else before the request, there is no need to do anything here
+    //if we do, we need to change that to resultState here
   };
 
   return { text, onClick, onReceive };
