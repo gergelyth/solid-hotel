00cc31b94e6c943ed2b8e9e7bb767971e7abe83a
Gergely Tóth <gergelytothdev@gmail.com>
2021-06-27 02:37:04 +0200

    [PMS] Add profile fake data

--- common/consts/hotelConsts.tsx
+++ common/consts/hotelConsts.tsx
@@ -1,4 +1,4 @@
 export const HotelName = "Paradise Hotel";
 //TODO anything using this should use the hotel WEbid details (getDefaultHotelDetails)
-export const HotelLocation = "Australia";
-export const HotelAddress = "3884 Sunrise Avenue, 43122, Perth";
+export const HotelLocation = "France";
+export const HotelAddress = "3884 Sunrise Avenue, 43122, Paris";
