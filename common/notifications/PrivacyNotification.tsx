af7a48b32b6a28003dd6d80524bfdb0c7ccf6cb9
Gergely Tóth <gergelytothdev@gmail.com>
2022-05-01 00:12:30 +0200

    [Privacy] Small cleanup - remove unused methods

--- common/notifications/PrivacyNotification.tsx
+++ common/notifications/PrivacyNotification.tsx
@@ -2,30 +2,10 @@ import { getSourceUrl, SolidDataset } from "@inrupt/solid-client";
 import {
   AddNotificationThingToDataset,
   CreateGuestPrivacyTokenDataset,
-  CreateHotelPrivacyTokenDataset,
 } from "../util/datasetFactory";
 import { NotificationType } from "../types/NotificationsType";
-import { HotelPrivacyToken } from "../types/HotelPrivacyToken";
-import { ConvertToHotelPrivacyToken } from "../hooks/usePrivacyTokens";
 import { ConvertToGuestPrivacyToken } from "../hooks/usePrivacyTokens";
 import { GuestPrivacyToken } from "../types/GuestPrivacyToken";
-
-//TODO can we improve the methods with generics to avoid duplication?
-export function DeserializeHotelPrivacyNotification(
-  dataset: SolidDataset
-): HotelPrivacyToken {
-  const datasetUrl = getSourceUrl(dataset);
-  if (!datasetUrl) {
-    throw new Error("Dataset URL is null");
-  }
-
-  const hotelPrivacyToken = ConvertToHotelPrivacyToken(dataset, datasetUrl);
-  if (!hotelPrivacyToken) {
-    throw new Error("Hotel privacy token cannot be null");
-  }
-
-  return hotelPrivacyToken;
-}
 
 export function DeserializeGuestPrivacyNotification(
   dataset: SolidDataset
@@ -41,19 +21,6 @@ export function DeserializeGuestPrivacyNotification(
   }
 
   return guestPrivacyToken;
-}
-
-export function SerializeHotelPrivacyNotification(
-  privacyToken: HotelPrivacyToken
-): SolidDataset {
-  const privacyTokenDataset = CreateHotelPrivacyTokenDataset(privacyToken);
-
-  const notificationDataset = AddNotificationThingToDataset(
-    privacyTokenDataset,
-    NotificationType.PrivacyToken
-  );
-
-  return notificationDataset;
 }
 
 export function SerializeGuestPrivacyNotification(
