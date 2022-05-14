befeb75f61d845fe4f336db9aad7d1d4ca3b9a44
Gergely Tóth <gergelytothdev@gmail.com>
2022-05-14 13:01:38 +0200

    [General] Replace deprecated function call

--- common/styles/theme.tsx
+++ common/styles/theme.tsx
@@ -1,14 +1,14 @@
-import { createMuiTheme } from "@material-ui/core/styles";
+import { createTheme } from "@material-ui/core/styles";
 import { red } from "@material-ui/core/colors";
 
 // Create a theme instance.
-const theme = createMuiTheme({
+const theme = createTheme({
   palette: {
     primary: {
       main: "#556cd6",
     },
     secondary: {
-      main: "#19857b",
+      main: "#B22222",
     },
     error: {
       main: red.A400,
