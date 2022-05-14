0af31ea6bdbb465d76753ac33c3599cb80b97137
Gergely Tóth <gergelytothdev@gmail.com>
2022-05-15 00:51:02 +0200

    [Setup] Add functionality to recognize project accounts and display options available specific for the currently logged in one

--- common/components/setup/setup-components.tsx
+++ common/components/setup/setup-components.tsx
@@ -8,11 +8,12 @@ import {
   withStyles,
 } from "@material-ui/core";
 import { ReactNode } from "react";
-import { Info, Backup, Save } from "@material-ui/icons";
+import { Info, Backup, Save, Done, Warning, Error } from "@material-ui/icons";
 import {
   GetDeserializeFunction,
   GetSerializeFunction,
 } from "../../setup/testDataButtons";
+import { green, red, yellow } from "@material-ui/core/colors";
 
 export type SetupButton = {
   buttonText: string;
@@ -125,5 +126,95 @@ export function TestDataSetupButtons({
         },
       ]}
     />
+  );
+}
+
+function TestAccountCheckTitle({
+  titleString,
+  color,
+  icon,
+}: {
+  titleString: string;
+  color: string;
+  icon: ReactNode;
+}): JSX.Element {
+  return (
+    <Grid
+      container
+      spacing={2}
+      alignItems="center"
+      justify="center"
+      direction="row"
+    >
+      <Grid item> {icon}</Grid>
+      <Grid item>
+        <Typography variant="h4" style={{ color: color }}>
+          <Box fontFamily="Monospace" fontWeight="fontWeightBold">
+            {titleString}
+          </Box>
+        </Typography>
+      </Grid>
+      <Grid item> {icon}</Grid>
+    </Grid>
+  );
+}
+
+export function TestAccountCheck({
+  type,
+  content,
+}: {
+  type: "success" | "warning" | "error";
+  content: JSX.Element;
+}): JSX.Element {
+  let title: JSX.Element | null = null;
+  let color: string;
+  switch (type) {
+    case "success":
+      color = green[600];
+      title = (
+        <TestAccountCheckTitle
+          titleString="ACCEPTED"
+          color={color}
+          icon={<Done style={{ color: color }} />}
+        />
+      );
+      break;
+    case "warning":
+      color = yellow[600];
+      title = (
+        <TestAccountCheckTitle
+          titleString="WARNING"
+          color={color}
+          icon={<Warning style={{ color: color }} />}
+        />
+      );
+      break;
+    case "error":
+      color = red[600];
+      title = (
+        <TestAccountCheckTitle
+          titleString="ERROR"
+          color={color}
+          icon={<Error style={{ color: color }} />}
+        />
+      );
+      break;
+  }
+
+  return (
+    <Grid item>
+      <Box border={5} p={3} borderRadius={16} borderColor={color}>
+        <Grid
+          container
+          spacing={2}
+          alignItems="center"
+          justify="center"
+          direction="column"
+        >
+          <Grid item>{title}</Grid>
+          <Grid item>{content}</Grid>
+        </Grid>
+      </Box>
+    </Grid>
   );
 }
--- common/pages/index.tsx
+++ common/pages/index.tsx
@@ -22,10 +22,123 @@ import { DeleteForever, FolderOpen, Person } from "@material-ui/icons";
 import HomeIcon from "@material-ui/icons/Home";
 import {
   SetupButtonGroup,
+  TestAccountCheck,
   TestDataSetupButtons,
 } from "../components/setup/setup-components";
 
+const HotelTestAccount = HotelWebId;
+/* TODO: change this to solidguest! */
+const GuestTestAccount = "https://gergelyth.inrupt.net/profile/card#me";
+
+function HotelSetupSubPage(): JSX.Element {
+  return (
+    <Grid
+      container
+      spacing={3}
+      alignItems="center"
+      justify="center"
+      direction="column"
+    >
+      <SetupButtonGroup
+        title={"Clear"}
+        buttons={[
+          {
+            buttonText: "Clear all but rooms",
+            onClick: GetClearAllButRoomsFunction(),
+            icon: <DeleteForever />,
+          },
+          {
+            buttonText: "Clear rooms",
+            onClick: GetClearAllRoomsFunction(),
+            icon: <DeleteForever />,
+          },
+        ]}
+        isClearGroup
+      />
+      <SetupButtonGroup
+        title={"Manual setup"}
+        buttons={[
+          {
+            buttonText: "Empty setup",
+            onClick: GetCreateEmptySetupFunction(),
+            icon: <FolderOpen />,
+          },
+          {
+            buttonText: "Setup hotel profile",
+            onClick: GetSetupHotelProfileFunction(),
+            icon: <Person />,
+            info: (
+              <Box>
+                <Typography variant="subtitle2">
+                  Hotel WebId: {HotelWebId}
+                </Typography>
+                <Typography variant="subtitle2">
+                  Hotel name: {HotelName}
+                </Typography>
+                <Typography variant="subtitle2">
+                  Hotel location: {HotelLocation}
+                </Typography>
+              </Box>
+            ),
+          },
+          {
+            buttonText: "Add rooms",
+            onClick: GetAddRoomsFunction(),
+            icon: <HomeIcon />,
+          },
+        ]}
+      />
+      <TestDataSetupButtons
+        zipName="serializedHotel.zip"
+        foldersOfInterest={HotelFoldersOfInterest}
+      />
+    </Grid>
+  );
+}
+
+function GuestSetupSubPage(): JSX.Element {
+  return (
+    <Grid
+      container
+      spacing={3}
+      alignItems="center"
+      justify="center"
+      direction="column"
+    >
+      <SetupButtonGroup
+        title={"Clear"}
+        buttons={[
+          {
+            buttonText: "Clear everything",
+            onClick: GetGuestClearEverythingFunction(),
+            icon: <DeleteForever />,
+          },
+        ]}
+        isClearGroup
+      />
+      <SetupButtonGroup
+        title={"Manual setup"}
+        buttons={[
+          {
+            buttonText: "Empty setup",
+            onClick: GetGuestEmptySetupFunction(),
+            icon: <FolderOpen />,
+          },
+        ]}
+      />
+      <TestDataSetupButtons
+        zipName="serializedGuest.zip"
+        foldersOfInterest={GuestFoldersOfInterest}
+      />
+    </Grid>
+  );
+}
+
 export default function Home(): JSX.Element {
+  const webId = GetSession()?.info.webId;
+  const isHotelTestAccount = webId === HotelTestAccount;
+  const isGuestTestAccount = webId === GuestTestAccount;
+
   return (
     <Grid
       container
@@ -41,124 +154,90 @@ export default function Home(): JSX.Element {
       <Grid item>
         <DynamicLoginComponent />
       </Grid>
-      <Grid item>
-        <Typography variant="body1">
-          Logged in as: {GetSession()?.info.webId ?? "NOT LOGGED IN"}
-        </Typography>
-      </Grid>
 
-      <Grid item>
-        <Typography variant="h6">Test accounts:</Typography>
-        <Typography variant="subtitle1">{HotelWebId}</Typography>
-        {/* TODO: change this! */}
-        <Typography variant="subtitle1">
-          https://gergelyth.inrupt.net/profile/card#me
-        </Typography>
-      </Grid>
+      {webId ? null : (
+        <Grid item>
+          <TestAccountCheck
+            type={"error"}
+            content={
+              <Typography variant="subtitle2" align="center">
+                Not logged in
+              </Typography>
+            }
+          />
+        </Grid>
+      )}
 
-      <Grid item>
-        <Typography variant="h6">Hotel Pod setup</Typography>
-      </Grid>
+      {isHotelTestAccount ? (
+        <Grid item>
+          <TestAccountCheck
+            type={"success"}
+            content={
+              <Box>
+                <Typography variant="subtitle2" align="center">
+                  Recognized as the hotel test account set up for this project.
+                </Typography>
+                <Typography variant="subtitle2" align="center">
+                  All functionalities available.
+                </Typography>
+              </Box>
+            }
+          />
+        </Grid>
+      ) : null}
 
-      <Grid
-        container
-        spacing={3}
-        alignItems="center"
-        justify="center"
-        direction="column"
-      >
-        <SetupButtonGroup
-          title={"Clear"}
-          buttons={[
-            {
-              buttonText: "Clear all but rooms",
-              onClick: GetClearAllButRoomsFunction(),
-              icon: <DeleteForever />,
-            },
-            {
-              buttonText: "Clear rooms",
-              onClick: GetClearAllRoomsFunction(),
-              icon: <DeleteForever />,
-            },
-          ]}
-          isClearGroup
-        />
-        <SetupButtonGroup
-          title={"Manual setup"}
-          buttons={[
-            {
-              buttonText: "Empty setup",
-              onClick: GetCreateEmptySetupFunction(),
-              icon: <FolderOpen />,
-            },
-            {
-              buttonText: "Setup hotel profile",
-              onClick: GetSetupHotelProfileFunction(),
-              icon: <Person />,
-              info: (
-                <Box>
-                  <Typography variant="subtitle2">
-                    Hotel WebId: {HotelWebId}
-                  </Typography>
-                  <Typography variant="subtitle2">
-                    Hotel name: {HotelName}
-                  </Typography>
-                  <Typography variant="subtitle2">
-                    Hotel location: {HotelLocation}
-                  </Typography>
-                </Box>
-              ),
-            },
-            {
-              buttonText: "Add rooms",
-              onClick: GetAddRoomsFunction(),
-              icon: <HomeIcon />,
-            },
-          ]}
-        />
-        <TestDataSetupButtons
-          zipName="serializedHotel.zip"
-          foldersOfInterest={HotelFoldersOfInterest}
-        />
-      </Grid>
+      {isGuestTestAccount ? (
+        <Grid item>
+          <TestAccountCheck
+            type={"success"}
+            content={
+              <Box>
+                <Typography variant="subtitle2" align="center">
+                  Recognized as the guest test account set up for this project.
+                </Typography>
+                <Typography variant="subtitle2" align="center">
+                  All functionalities available.
+                </Typography>
+              </Box>
+            }
+          />
+        </Grid>
+      ) : null}
 
-      <Grid item>
-        <Typography variant="h6">Guest Pod setup</Typography>
-      </Grid>
+      {webId && !isHotelTestAccount && !isGuestTestAccount ? (
+        <Grid item>
+          <TestAccountCheck
+            type={"warning"}
+            content={
+              <Box>
+                <Typography variant="subtitle2" align="center">
+                  Not recognized as an account set up for this project.
+                </Typography>
+                <Typography variant="subtitle2" align="center">
+                  Tread lightly.
+                </Typography>
+                <Typography variant="subtitle2" align="center">
+                  Prepared test data not applicable here.
+                </Typography>
+              </Box>
+            }
+          />
+        </Grid>
+      ) : null}
 
-      <Grid
-        container
-        spacing={3}
-        alignItems="center"
-        justify="center"
-        direction="column"
-      >
-        <SetupButtonGroup
-          title={"Clear"}
-          buttons={[
-            {
-              buttonText: "Clear everything",
-              onClick: GetGuestClearEverythingFunction(),
-              icon: <DeleteForever />,
-            },
-          ]}
-          isClearGroup
-        />
-        <SetupButtonGroup
-          title={"Manual setup"}
-          buttons={[
-            {
-              buttonText: "Empty setup",
-              onClick: GetGuestEmptySetupFunction(),
-              icon: <FolderOpen />,
-            },
-          ]}
-        />
-        <TestDataSetupButtons
-          zipName="serializedGuest.zip"
-          foldersOfInterest={GuestFoldersOfInterest}
-        />
-      </Grid>
+      {webId && !isGuestTestAccount ? (
+        <Grid item>
+          <Typography variant="h6">Hotel Pod setup</Typography>
+        </Grid>
+      ) : null}
+      {webId && !isGuestTestAccount ? <HotelSetupSubPage /> : null}
+
+      {webId && !isHotelTestAccount ? (
+        <Grid item>
+          <Typography variant="h6">Guest Pod setup</Typography>
+        </Grid>
+      ) : null}
+      {webId && !isHotelTestAccount ? <GuestSetupSubPage /> : null}
     </Grid>
   );
 }
